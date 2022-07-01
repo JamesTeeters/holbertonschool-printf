9b7788384a6db634eef061249a639630926fd712
JamesTeeters <4925@holbertonschool.com>
2022-07-01 08:58:52 -0700

    'created blank file that will house my printf code'

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _printf - function that prints a variabl number of arguments 
+ * of different data types to terminal.
+ * @format: input string containg various arguments.
+ *
+ * Return: interger
+ */
+int _printf(const char *format, ...)
+{
+
+}
