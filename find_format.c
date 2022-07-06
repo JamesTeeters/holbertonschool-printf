a8f2a4c8f406d602a1344d2d6a57ce154e9b38ce
JamesTeeters <4925@holbertonschool.com>
2022-07-05 17:24:11 -0700

    'changed form char ptr to char'

--- find_format.c
+++ find_format.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
  * find_func - finds correct format function from structure.
- * @p: input char.
+ * @c: input char.
  *
  * Return: int
  */
-int (*find_func(char *p))(va_list args)
+int (*find_func(char c))(va_list args)
 {
 	int i;
 
 	func_t func[] = {
-		{"c", printc},
-		{"s", prints}
+		{'c', printc},
+		{'s', prints}
 	};
 
 	i = 0;
-	while (func[i].ptr)
+	while (func[i].specifier)
 	{
-		if (func[i].ptr == p)
+		if (func[i].specifier == c)
 			return (func[i].f);
 		i++;
 	}
