f5eb768ca0fcc7ec3488875d1367b6549214a7b0
JamesTeeters <4925@holbertonschool.com>
2022-07-06 06:42:29 -0700

    'added count var to get proper return'

--- _printf.c
+++ _printf.c
@@ -8,6 +8,7 @@
  */
 int _printf(const char *format, ...)
 {
+	int count = 0;
 	unsigned int i;
 	va_list args;
 	int (*fun)(va_list args);
@@ -17,23 +18,27 @@ int _printf(const char *format, ...)
 	i = 0;
 	while (format[i] != '\0')
 	{
-		for (i = i; format[i] != '%' && format[i] != '\0'; i++)
+		for (i = i; format[i] != '%' && format[i]; i++)
+		{
 			_putchar(format[i]);
+			count++;
+		}
 		if (format[i + 1] == '%')
 		{
 			_putchar('%');
 			i++;
+			count++;
 		}
 		if (format[i + 1] == 'c' || format[i + 1] == 's')
 		{
 			fun = find_func(format[i + 1]);
-			fun(args);
+			count += fun(args);
 			i++;
 		}
 		if (format[i] != '\0')
 			i++;
 	}
 	va_end(args);
-	return (i);
+	return (count);
 }
 
